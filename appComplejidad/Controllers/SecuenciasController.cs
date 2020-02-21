c49c9190909c3cc3594798e98c15b3d9e4e8f972
Juan Camilo Gil Cordoba <juan.gil9768@uco.net.co>
2020-02-20 21:31:49 -0500

    secuencias

---
+++ appComplejidad/Controllers/SecuenciasController.cs
@@ -0,0 +1,99 @@
+﻿using appComplejidad.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace appComplejidad.Controllers
+{
+    public class SecuenciasController : Controller
+    {
+        // GET: Secuencias
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Secuencia()
+        {
+            Secuencia aSecuen = new Secuencia();
+            System.Web.HttpContext.Current.Session["HashCode"] = aSecuen.Id;
+            System.Web.HttpContext.Current.Session["Secuencia"] = aSecuen.Elemento;
+            System.Web.HttpContext.Current.Session["SecuenciaReverse"] = aSecuen.ElementoReverse;
+            return View();
+        }
+
+        // GET: Secuencias/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Secuencias/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Secuencias/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Secuencias/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Secuencias/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Secuencias/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Secuencias/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ appComplejidad/Models/Secuencia.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace appComplejidad.Models
+{
+    public class Secuencia
+    {
+
+        public Secuencia()
+        {
+            this.Elemento = new List<double>();
+            this.ElementoReverse = new List<double>();
+            this.Elemento.Add(2);
+            this.Elemento.Add(4);
+            this.Elemento.Add(5);
+            this.Elemento.Add(6);
+
+            int n = Elemento.Count;
+            int a = n-1;
+            for (int i=0; i<n; i++){
+                this.ElementoReverse.Add(this.Elemento[a]);
+                a--;
+            }
+            
+        }
+
+        public int Id { get; set; }
+        public List<double> Elemento { get; set; }
+        public List<double> ElementoReverse { get; set; }
+    }
+}
