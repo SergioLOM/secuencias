4fa26fc7d42319de271466521b47c97c4dda0081
Juan Camilo Gil Cordoba <juan.gil9768@uco.net.co>
2020-02-23 17:55:23 -0500

    Ordenando elementos

--- appComplejidad/Models/Secuencia.cs
+++ appComplejidad/Models/Secuencia.cs
@@ -13,11 +13,23 @@ namespace appComplejidad.Models
             this.Elemento = new List<double>();
             this.ElementoReverse = new List<double>();
             this.Elemento.Add(2);
+            this.Elemento.Add(7);
             this.Elemento.Add(4);
-            this.Elemento.Add(5);
-            this.Elemento.Add(6);
-
+            this.Elemento.Add(8);
+            this.Elemento.Add(7);
             int n = Elemento.Count;
+            double t;
+            for (int c = 1; c < n; c++)
+                for (int b = n - 1; b >= c; b--)
+                {
+                    if (Elemento[b - 1] > Elemento[b])
+                    {
+                        t = Elemento[b - 1];
+                        Elemento[b - 1] = Elemento[b];
+                        Elemento[b] = t;
+                    }
+                }
+
             int a = n-1;
             for (int i=0; i<n; i++){
                 this.ElementoReverse.Add(this.Elemento[a]);
@@ -29,5 +41,6 @@ namespace appComplejidad.Models
         public int Id { get; set; }
         public List<double> Elemento { get; set; }
         public List<double> ElementoReverse { get; set; }
+        
     }
 }
